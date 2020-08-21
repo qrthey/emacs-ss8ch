689b11fabfc9e8b58b362e4cbbdee39d430b3dd1
qrt <khconsultancy@gmail.com>
2020-08-21 17:19:08 +0200

    Minor non functional refactorings

--- ss8ch.el
+++ ss8ch.el
@@ -1,14 +1,14 @@
 ;; ssh agent interop
 ;; -----------------
-(defvar ss8ch-agent-socket-var "SSH_AUTH_SOCK")
-(defvar ss8ch-agent-process-id "SSH_AGENT_PID")
-(defvar ss8ch-agent-search-end "; export")
+(defconst ss8ch-agent-socket-var "SSH_AUTH_SOCK")
+(defconst ss8ch-agent-process-id "SSH_AGENT_PID")
+(defconst ss8ch-agent-search-end "; export")
 
 (defun ss8ch-find-var-value-in-agent-response
     (var-name response)
   "Takes a var-name and the response of calling `ssh-agent` in a
-  shell environment. Finds the value for the given var-name in
-  the given agent response."
+   shell environment. Finds the value for the given var-name in
+   the given agent response."
   (let* ((start-idx (+ 1
                        (length var-name)
                        (string-match var-name response)))
@@ -18,8 +18,10 @@
     (substring response start-idx end-idx)))
 
 (defun ss8ch-ensure-agent ()
+  "Checks if the environment contains the pid var for an ssh
+   agent. If not so, starts an ssh-agent process and captures its
+   output the configure the environment."
   (when (not (getenv ss8ch-agent-process-id))
-    (message "ss8ch-... starting new ssh-agent")
     (let ((agent-response (shell-command-to-string "ssh-agent")))
       (setenv ss8ch-agent-socket-var
               (ss8ch-find-var-value-in-agent-response
@@ -28,17 +30,18 @@
       (setenv ss8ch-agent-process-id
               (ss8ch-find-var-value-in-agent-response
                ss8ch-agent-process-id
-               agent-response))
-      (message "ssh agent started"))))
+               agent-response)))
+    (message "ss8ch ~ agent started")))
 
 (defun ss8ch-handle-passphrase-request (process process-message)
-  "Helper to handle passphrase requests from running processes.
-   To be registered with set-process-filter."
+  "Helper function to handle passphrase requests from the ssh-add
+   process."
   (save-match-data
     (if (string-match "passphrase.*:\\s *\\'" process-message)
         (process-send-string process
                              (concat (read-passwd process-message) "\n"))
-      (message process-message))))
+      (if (not (string-match "^\n+$" process-message))
+          (message (concat "ss8ch ~ " (string-trim process-message)))))))
 
 (defun ss8ch-add (key-file)
   "Checks if an agent is registered in the environment. If not
